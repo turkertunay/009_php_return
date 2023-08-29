e1ff70651d740ed4e8495a911ba6bede2c1d1682
turkertunay <75833839+turkertunay@users.noreply.github.com>
2023-08-29 14:09:28 +0300

    Add files via upload

---
+++ 009_return.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+
+  <?php
+      function cube($num){
+          return $num * $num * $num;
+          }
+      $cubeResult = cube(11);
+      echo $cubeResult;
+  ?>
+  </body>
+</html>
